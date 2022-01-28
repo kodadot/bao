2ae23ce1af1e828a51c3b79adc1b4a9a116e84a9
Viki Val <viktorko99@gmail.com>
2022-01-28 15:24:28 +0100

    :coffin: :broom: removing unused code

--- minimal-cf-images/main.py
+++ minimal-cf-images/main.py
@@ -1,6 +1,6 @@
 
 import asyncio
-from logging import info, basicConfig, INFO
+from logging import info, basicConfig, INFO, raiseExceptions
 from async_lib import dispatch
 from fetch import fetch_last_minted_nfts, map_fetch_one
 from my_queue import add_task
@@ -15,18 +15,10 @@ signal(SIGINT, handle_sigint)
 async def async_init(jozo):
   meta = await fetch_last_minted_nfts()
   for item in meta:
-    # print('Priting x: ', item)
     await add_task(map_fetch_one(item), jozo)
-  
-  # for i in range(START_AT, START_AT + OFFSET):
-  #   with open(f'missing/chunk{i}.json') as f:
-  #     meta = load(f)
-  #   info(f'[ASYNC INIT]: 🎲 Starting at {i} of {START_AT + OFFSET}')
-  #   # mapped = list(map(map_fetch_one, meta))
-  #   for item in meta:
-  #       await add_task(map_fetch_one(item), jozo)
 
 if __name__ == '__main__':
   basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=INFO, datefmt='%H:%M:%S')
+  # raiseExceptions = False # only for the runner
   info('[APP]: Running')
   asyncio.run(dispatch(async_init), debug=True)
