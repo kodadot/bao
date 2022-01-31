46a16b49f05d4218788eebe5aa692c73969fac5b
Martin Tonhauzer <martin.tonhauzer@nbu.gov.sk>
2022-01-31 22:15:33 +0100

    removed unused packages

--- minimal-cf-images/my_queue.py
+++
@@ -1,9 +0,0 @@
-
-Jozo = None
-
-async def add_task(task, jozo = None):
-    global Jozo
-    assert jozo is not None or Jozo is not None
-    Jozo = Jozo or jozo
-    jozo = jozo or Jozo
-    await jozo.put(task)
