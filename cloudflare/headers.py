0b4b7158fa339fea3b5766e65d8ae89b65c6381e
Viki Val <viktorko99@gmail.com>
2023-01-04 15:48:16 +0100

    :bug: bad headers

--- cloudflare/headers.py
+++ cloudflare/headers.py
@@ -4,7 +4,7 @@ from os import getenv
 load_dotenv()
 ACCOUNT = 'b3f9fdfd827152316d080a5ddee59915'
 # ACCOUNT = getenv('ACCOUNT')
-API_KEY = getenv('CF_API_KEY')
+API_KEY = getenv('API_KEY')
 
 CF_IMAGES_URI = f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT}/images/v1"
 HEADERS={"Authorization": f"Bearer {API_KEY}"}
